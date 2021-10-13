9042f24495ec6aa5e548f63edede7fbebcb4f75d
Bunyasit Fang <big168bk@gmail.com>
2021-10-13 14:34:53 +0700

    added button and styled-components

---
+++ src/features/account/components/account.styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components/native";
+import { TextInput } from "react-native-paper";
+import { Platform } from "react-native";
+import AwesomeButtonC from "react-native-really-awesome-button/src/themes/c137";
+import { Button } from "react-native-paper";
+
+export const AccountBackground = styled.ImageBackground.attrs({
+  source: require("../../../../assets/splash.png"),
+})`
+  flex: 1;
+  background-color: #020302;
+  justify-content: center;
+`;
+
+export const AccountCover = styled.View`
+  position: absolute;
+  top: 45%;
+  width: 80%;
+  height: 30%;
+  background-color: rgba(255, 255, 255, 0);
+`;
+
+export const Container = styled.View`
+  padding: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[4]};
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 50px;
+  align-items: center;
+  align-self: center;
+`;
+
+export const AuthButton = styled(AwesomeButtonC).attrs({})`
+  align-self: center;
+`;
+
+export const AuthInput = styled(TextInput)`
+  ${Platform.OS === "android" ? "width: 250px" : "width: 300px"}
+`;
---
+++ src/features/account/screens/register.screen.js
@@ -0,0 +1,89 @@
+import React, { useState, useContext } from "react";
+import { Text } from "../../../components/typography/text.component";
+import styled from "styled-components/native";
+import { TextInput } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
+
+import {
+  AccountBackground,
+  AccountCover,
+  Container,
+  AuthInput,
+  AuthButton,
+} from "../components/account.styles";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import AwesomeButtonC from "react-native-really-awesome-button/src/themes/c137";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+const Center = styled.View``;
+
+export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+  const { onRegister, error, isLoading } = useContext(AuthenticationContext);
+  return (
+    <AccountBackground>
+      <Container>
+        {error && (
+          <Spacer position="bottom" size="medium">
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Center>
+          <AuthInput
+            label="Email"
+            value={email}
+            textContentType="emailAddress"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Spacer size="large" />
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Spacer size="large" />
+          <AuthInput
+            label="Repeat Password"
+            value={repeatedPassword}
+            textContentType="password"
+            secureTextEntry
+            onChangeText={(text) => setRepeatedPassword(text)}
+          />
+          <Spacer size="large" />
+          {!isLoading ? (
+            <AuthButton
+              type="primary"
+              size="large"
+              onPress={() => {
+                console.log(email, password);
+                onRegister(email, password, repeatedPassword);
+                console.log(error);
+              }}
+            >
+              Register
+            </AuthButton>
+          ) : (
+            <ActivityIndicator animating={true} color={Colors.blue100} />
+          )}
+        </Center>
+      </Container>
+      <Spacer size="large" />
+      <Spacer size="large" />
+      <AuthButton
+        type="secondary"
+        size="medium"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      >
+        Back
+      </AuthButton>
+    </AccountBackground>
+  );
+};
