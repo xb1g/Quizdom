a27997681d55f0071ae03dd3d4977c0592fc4fa3
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-10-13 21:15:36 +0700

    Need client ID from firebase sucks

--- src/features/home/screens/LoginScreen.js
+++ src/features/home/screens/LoginScreen.js
@@ -1,10 +1,26 @@
 import React, {component} from "react";
-import {View,Text,Stylesheet} from 'react-native';
+import {View,Text,Stylesheet,Button} from 'react-native';
 class LoginScreen extends component{
+    signInWithGoogleAsync=async()=>{
+        try{
+            const result=await Expo.Google.logInAsync({
+                //androidClientId:YOUR_CLIENT_ID_HERE,
+                //iosClientId:YOUR_CLIENT_ID_HERE,
+                scopes:["profile","email"]
+            });
+            if (result.type ==='success'){
+                return result.accessToken;
+            } else{
+                return {cancelled:true};
+            } 
+        }catch (e) {
+                return {error:true};
+        }
+    };
     render(){
         return(
             <View style={styles.container}>
-                <Text>Login Screen</Text>
+                <Button title="Sign in with Google" onPress={()=>this.signInWithGoogleAsync()}/>
             </View>
         );
     }
