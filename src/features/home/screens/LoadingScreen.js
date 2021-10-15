e94f11526816d51434f056a8ed5e8639a6e6b606
Bunyasit Fang <big168bk@gmail.com>
2021-10-15 10:12:07 +0700

    prettier

--- src/features/home/screens/LoadingScreen.js
+++ src/features/home/screens/LoadingScreen.js
@@ -1,35 +1,34 @@
-import React, {component} from "react";
-import {View,Text,Stylesheet,ActivityIndicator} from 'react-native';
-import firebase from 'firebase'
-class LoadingScreen extends component{
-    componentDidMount(){
-        this.checkIfLoggedIn();
-    }
-    checkIfLoggedIn=()=>{
-        firebase.auth().onAuthStateChanged(
-            function(user){
-            if(user){
-                this.props.navigation.navigate('DashboardScreen');
-            }
-            else{
-                this.props.navigation.navigate('LoginScreen');
-            }
-        }.bind(this)
-        );
-    }
-    render(){
-        return(
-            <View style={styles.container}>
-                <ActivityIndicator size="large" />
-            </View>
-        );
-    }
+import React, { component } from "react";
+import { View, Text, Stylesheet, ActivityIndicator } from "react-native";
+import firebase from "firebase";
+class LoadingScreen extends component {
+  componentDidMount() {
+    this.checkIfLoggedIn();
+  }
+  checkIfLoggedIn = () => {
+    firebase.auth().onAuthStateChanged(
+      function (user) {
+        if (user) {
+          this.props.navigation.navigate("DashboardScreen");
+        } else {
+          this.props.navigation.navigate("LoginScreen");
+        }
+      }.bind(this)
+    );
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 }
 export default LoadingScreen;
-const styles=Stylesheet.create({
-    container:{
-        flex:1,
-        alignItem:'center',
-        justifyContent: 'center'
-    }
-})
+const styles = Stylesheet.create({
+  container: {
+    flex: 1,
+    alignItem: "center",
+    justifyContent: "center",
+  },
+});
