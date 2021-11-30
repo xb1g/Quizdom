6546546c1b03b7735b64d34c661c1d17485d4978
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-11-30 20:25:24 +0700

    nothing

--- src/services/data/userdata_class.js
+++ src/services/data/userdata_class.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, createContext } from "react";
-
 import { db, auth } from "../../../firebase-config";
 import {
   collection,
@@ -9,31 +8,21 @@ import {
   onSnapshot,
 } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
+
 class User_data{
-    constructor(email,password,name,lastname,username,year_of_birth){
-        this.email=email;
-        this.password=password;
-        this.name=name;
-        this.lastname=lastname;
-        this.username=username;
-        this.year_of_birth=year_of_birth;
-        this.email=String(this.email);
-        this.password=String(this.password);
-        this.name=String(this.name);
-        this.lastname=String(this.lastname);
-        this.username=String(this.username);
+    constructor(nothing){
+        this.nothing=nothing
     }
-    map_info(this,milestones,exp,level,current_theme,achievement,map_decor){
+    map(this,milestones,exp,level,current_theme,achievement,map_decor){
         this.milestones=milestones
         this.exp=exp
         this.level=level
         this.current_theme=current_theme
         this.achievement=achievement
         this.map_decor=map_decor
-        this.map_1m=[this.milestons,this.exp,this.level]
     }
-    user_profile_info(this,friend,achievement_pg,about_me,theme,questions_data,profile_pic,map_pg,subject_like,fav_badge,frame,banner){
-        this.friend=friend
+    user(this,friend,achievement_pg,about_me,theme,questions_data,profile_pic,map_pg,subject_like,fav_badge,frame,banner){
+        this.friend=friend //already have
         this.achievement_pg=achievement_pg
         this.about_me=about_me
         this.theme=theme
@@ -45,7 +34,7 @@ class User_data{
         this.frame=frame
         this.banner=banner
     }
-    plan_info(this,name_plan,map_in_p){
+    plan(this,name_plan,map_in_p){
         this.name_plan=name_plan
         this.map_in_p=map_in_p
     }
