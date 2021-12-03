a66ef4ce65598f83892e985bdfdf8c389b4fe7cf
Bunyasit Fang <big168bk@gmail.com>
2021-12-03 19:29:36 +0700

    many things + theme

--- src/infrastructure/navigation/community.navigator.js
+++ src/infrastructure/navigation/community.navigator.js
@@ -5,12 +5,18 @@ import {
 } from "@react-navigation/stack";
 import { Button } from "../../features/planner/components/button.component";
 import { shadow } from "../../components/shadow/shadow.styles";
-
+import { Ionicons } from "@expo/vector-icons";
 import { CommunityScreen } from "../../features/community/screens/community.screen";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Platform, View } from "react-native";
+import { useTheme } from "styled-components/native";
 
 const CommunityStack = createStackNavigator();
 
 export const CommunityNavigator = ({ navigation }) => {
+  const insets = useSafeAreaInsets();
+  const theme = useTheme();
+  console.log(theme);
   return (
     <CommunityStack.Navigator
       initialRouteName="Planner"
@@ -25,22 +31,33 @@ export const CommunityNavigator = ({ navigation }) => {
         options={{
           title: "Community ",
           headerTitleStyle: {
-            position: "absolute",
-            left: -190,
-            top: -5,
+            marginLeft: Platform.OS === "android" ? 0 : -50,
+            marginTop: -10,
             alignSelf: "flex-start",
             fontSize: 50,
             justifyContent: "flex-start",
             fontFamily: "Airstrike",
             color: "black",
           },
-          headerRight: () => <Button navigation={navigation} />,
-
+          headerRight: () => (
+            <View
+              style={{
+                marginRight: 20,
+                marginBottom: 10,
+                backgroundColor: theme.colors.brand.primary,
+                padding: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Ionicons name="ios-search" size={30} color="black" />
+            </View>
+          ),
           headerStyle: {
-            backgroundColor: "white",
+            backgroundColor: theme.colors.bg.primary,
             borderBottomRightRadius: 30,
             borderBottomLeftRadius: 30,
-            height: 110,
+            height: 60 + insets.top,
             ...shadow.shadow2,
           },
         }}
