85dc2525dfab21f9f10c805d7bffac6eb7b31b1d
Bunyasit Fang <big168bk@gmail.com>
2021-12-09 08:23:03 +0700

    svg reanimatedf chart

--- src/components/visualization/circular-progress.component.js
+++ src/components/visualization/circular-progress.component.js
@@ -10,9 +10,6 @@ import {
 import Constants from "expo-constants";
 import Svg, { G, Circle, Rect } from "react-native-svg";
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
-
 export const CircularProgress = ({
   percentage = 75,
   radius = 40,
@@ -28,44 +25,6 @@ export const CircularProgress = ({
   const inputRef = React.useRef();
   const circumference = 2 * Math.PI * radius;
   const halfCircle = radius + strokeWidth;
-
-  const animation = (toValue) => {
-    return Animated.timing(animated, {
-      delay: 1000,
-      toValue,
-      duration,
-      useNativeDriver: true,
-      easing: Easing.out(Easing.ease),
-    }).start(() => {
-      animation(toValue === 0 ? percentage : 0);
-    });
-  };
-
-  React.useEffect(() => {
-    animation(percentage);
-    animated.addListener(
-      (v) => {
-        const maxPerc = (100 * v.value) / max;
-        const strokeDashoffset =
-          circumference - (circumference * maxPerc) / 100;
-        if (inputRef?.current) {
-          inputRef.current.setNativeProps({
-            text: `${Math.round(v.value)}`,
-          });
-        }
-        if (circleRef?.current) {
-          circleRef.current.setNativeProps({
-            strokeDashoffset,
-          });
-        }
-      },
-      [max, percentage]
-    );
-
-    return () => {
-      animated.removeAllListeners();
-    };
-  });
 
   return (
     <View style={{ width: radius * 2, height: radius * 2 }}>
@@ -99,21 +58,6 @@ export const CircularProgress = ({
           />
         </G>
       </Svg>
-      <AnimatedTextInput
-        ref={inputRef}
-        underlineColorAndroid="transparent"
-        editable={false}
-        defaultValue="0"
-        style={[
-          StyleSheet.absoluteFillObject,
-          { fontSize: radius / 2, color: textColor ?? color },
-          styles.text,
-        ]}
-      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  text: { fontWeight: "900", textAlign: "center" },
-});
