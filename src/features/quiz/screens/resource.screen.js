69a8e3097df5ed9106e2fc655accfeab9273c510
Bunyasit Fang <big168bk@gmail.com>
2021-12-10 19:16:25 +0700

    choose choice out now

--- src/features/quiz/screens/resource.screen.js
+++ src/features/quiz/screens/resource.screen.js
@@ -9,91 +9,56 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeTop } from "../../../components/utility/safe-area.component";
 import styled, { useTheme } from "styled-components/native";
 import AwesomeButtonC from "react-native-really-awesome-button/src/themes/c137";
+import { HeaderText } from "../../../components/utility/header-text.component";
 
 const ResourceItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.secondary};
-  border-radius: 50px;
+  border-radius: 15px;
   margin-bottom: ${(props) => props.theme.space[3]};
   margin-left: ${(props) => props.theme.space[3]};
   margin-right: ${(props) => props.theme.space[3]};
 `;
-const QuizStartItem = styled(List.Item)`
+const QuizStartItem = styled(TouchableOpacity)`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.brand.primary};
   border-radius: 20px;
-  margin-bottom: ${(props) => props.theme.space[3]};
-  margin-left: ${(props) => props.theme.space[0]};
-  margin-right: ${(props) => props.theme.space[0]};
+  margin: ${(props) => props.theme.space[3]};
+  margin-left: auto;
 `;
 
 export function ResourceScreen({ navigation }) {
   const theme = useTheme();
   const { selectedModule } = useContext(MapsContext);
   return (
-    <>
-      <SafeTop>
-        <BackButton navigation={navigation} />
-        <View style={{ flex: 1, backgroundColor: "#393939" }}>
-          <View /*style={styles.container}*/>
-            <Text
-              variant="label"
-              style={{
-                color: "white",
-                fontSize: 36,
-                marginLeft: 60,
-                marginTop: 50,
-              }}
-            >
-              {"Study" + " " + "Resources"}
-            </Text>
-          </View>
-          <ResourceItem
-            style={{
-              backgroundColor: theme.colors.bg.secondary,
-              borderRadius: 20,
-              marginHorizontal: 10,
-              marginTop: 20,
-            }}
-            titleStyle={{
-              color: theme.colors.brand.primary,
-              fontSize: 20,
-            }}
-            title={"https://www.google.co.th/?hl=th"}
-          />
-        </View>
-        <View style={{ flex: 0, backgroundColor: "#393939" }}>
-          <Text
-            style={{
-              color: "#ffffff",
-              marginBottom: 20,
-              fontSize: 25,
-              marginLeft: 20,
-            }}
-          >
-            {selectedModule}
-          </Text>
-
-          <TouchableOpacity>
-            <QuizStartItem
-              style={{
-                backgroundColor: "#ffc8ff",
-                marginLeft: 225,
-                paddingRight: 10,
-                borderRadius: 20,
-                marginTop: 20,
-                marginBottom: 50,
-              }}
-              titleStyle={{
-                color: "white",
-                fontSize: 28,
-              }}
-              title="QUZIZ"
-              onPress={() => navigation.navigate("Quiz")}
-            />
-          </TouchableOpacity>
-        </View>
-      </SafeTop>
-    </>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.bg.primary,
+      }}
+    >
+      <HeaderText
+        title={selectedModule}
+        style={{
+          color: "#245",
+        }}
+      />
+      <BackButton navigation={navigation} />
+      <SafeTop />
+      <View style={{ flex: 1, marginTop: 50 }}>
+        <ResourceItem
+          titleStyle={{
+            color: theme.colors.brand.primary,
+            fontSize: 20,
+          }}
+          title={"https://www.google.co.th/?hl=th"}
+        />
+      </View>
+      <View style={{ flex: 0, backgroundColor: "#393939" }}>
+        <QuizStartItem onPress={() => navigation.navigate("Quiz")}>
+          <Text>QUIZ</Text>
+        </QuizStartItem>
+      </View>
+    </View>
   );
 }
