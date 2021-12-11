6cfac8b25d6ce9143146abf5e469dd561c8389be
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-12-11 22:45:58 +0700

    index

--- src/services/data/math/sets/index.js
+++ src/services/data/math/sets/index.js
@@ -1,5 +1,6 @@
 import basic_of_set from "./resource_basic_of_set.json";
-
+import set_types from "./resource_set_types.json";
 export const setResource = {
   "Basic of Set": basic_of_set,
+  "Set types": set_types,
 };
