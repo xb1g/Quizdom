ccb50c4d2fd84a63f782dd404323e268b3757871
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-12-13 21:44:43 +0700

    import text.component

--- src/infrastructure/navigation/planner.navigator.js
+++ src/infrastructure/navigation/planner.navigator.js
@@ -1,57 +1,37 @@
 import React from "react";
+import { View, TouchableOpacity, StyleSheet, Linking } from "react-native";
+import styled, { useTheme } from "styled-components/native";
 import {
   createStackNavigator,
   TransitionPresets,
 } from "@react-navigation/stack";
+import { List } from "react-native-paper";
+import { Text } from "../../components/typography/text.component";
 import { PlannerScreen } from "../../features/planner/screens/planner.screen";
 import { AddPlanScreen } from "../../features/planner/screens/add-plan.screen";
 import { Button } from "../../features/planner/components/button.component";
 import { shadow } from "../../components/shadow/shadow.styles";
 
 const PlannerStack = createStackNavigator();
-
+const PlanningItem = styled(List.Item)`
+  margin-top: ${(props) => props.theme.space[3]};
+  margin-bottom: ${(props) => props.theme.space[3]};
+`;
 export const PlannerNavigator = ({ navigation }) => {
+  const theme = useTheme();
   return (
-    <PlannerStack.Navigator
-      initialRouteName="PlannerScreen"
-      screenOptions={{
-        // headerShown: false,
-        ...TransitionPresets.ModalPresentationIOS,
-      }}
-    >
-      <PlannerStack.Screen
-        name="PlannerScreen"
-        component={PlannerScreen}
-        options={{
-          title: "Plan ",
-          headerTitleStyle: {
-            position: "absolute",
-            left: -180,
-            top: -5,
-            alignSelf: "flex-start",
-            fontSize: 50,
-            justifyContent: "flex-start",
-            fontFamily: "Airstrike",
-            color: "black",
-          },
-          headerRight: () => <Button navigation={navigation} />,
-
-          headerStyle: {
-            backgroundColor: "white",
-            borderBottomRightRadius: 30,
-            borderBottomLeftRadius: 30,
-            // height: 110,
-            ...shadow.shadow2,
-          },
+    <>
+      <Text
+        variant="label"
+        style={{
+          color: "white",
+          fontSize: 60,
+          marginHorizontal: "auto",
+          backgroundColor: theme.colors.bg.primary,
         }}
-      />
-      <PlannerStack.Screen
-        name="AddPlan"
-        component={AddPlanScreen}
-        options={{
-          gestureResponseDistance: 400,
-        }}
-      />
-    </PlannerStack.Navigator>
+      >
+        {"Plan" + " "}
+      </Text>
+    </>
   );
 };
