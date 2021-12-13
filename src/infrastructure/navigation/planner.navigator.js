95dfe0d647ad1ea77123061890118c411f0cbae9
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-12-13 22:49:28 +0700

    1 plan

--- src/infrastructure/navigation/planner.navigator.js
+++ src/infrastructure/navigation/planner.navigator.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet, Linking } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Linking,
+  Dimensions,
+  TouchableHighlight,
+} from "react-native";
 import styled, { useTheme } from "styled-components/native";
 import {
   createStackNavigator,
@@ -11,11 +18,15 @@ import { PlannerScreen } from "../../features/planner/screens/planner.screen";
 import { AddPlanScreen } from "../../features/planner/screens/add-plan.screen";
 import { Button } from "../../features/planner/components/button.component";
 import { shadow } from "../../components/shadow/shadow.styles";
+import { ScrollView } from "react-native-gesture-handler";
 
 const PlannerStack = createStackNavigator();
+
 const PlanningItem = styled(List.Item)`
   margin-top: ${(props) => props.theme.space[3]};
   margin-bottom: ${(props) => props.theme.space[3]};
+  margin-right: ${(props) => props.theme.space[3]};
+  margin-left: ${(props) => props.theme.space[3]};
 `;
 export const PlannerNavigator = ({ navigation }) => {
   const theme = useTheme();
@@ -26,12 +37,58 @@ export const PlannerNavigator = ({ navigation }) => {
         style={{
           color: "white",
           fontSize: 60,
-          marginHorizontal: "auto",
-          backgroundColor: theme.colors.bg.primary,
+          paddingHorizontal: 100,
+          backgroundColor: "#138000",
+          paddingTop: 20,
+          paddingBottom: 20,
         }}
       >
         {"Plan" + " "}
       </Text>
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#393939",
+          }}
+        >
+          <View style={{ marginTop: 30, marginHorizontal: 50 }}>
+            <TouchableHighlight
+              style={{
+                borderRadius:
+                  Math.round(
+                    Dimensions.get("window").width +
+                      Dimensions.get("window").height
+                  ) / 2,
+                width: Dimensions.get("window").width * 0.2,
+                height: Dimensions.get("window").width * 0.2,
+                backgroundColor: "#ffc8ff",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              underlayColor="#ffffff"
+              onPress={() => {
+                console.log("pressed sets plan");
+              }}
+            >
+              <Text style={{ fontSize: 36, color: "white" }}> {"Sets"} </Text>
+            </TouchableHighlight>
+          </View>
+
+          <PlanningItem
+            style={{
+              marginHorizontal: 30,
+              backgroundColor: "#ffc8ff",
+              borderRadius: 30,
+            }}
+            titleStyle={{
+              color: "white",
+              fontSize: "20",
+            }}
+            title="Start studying : 17/11/21"
+          />
+        </View>
+      </ScrollView>
     </>
   );
 };
