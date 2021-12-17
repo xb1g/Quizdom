216ef3e079deb34caeda8851ffff6e36bbcf1c99
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-12-17 21:15:54 +0700

    context

---
+++ src/services/quiz/quiz.context.js
@@ -0,0 +1,27 @@
+import React, { createContext, useEffect, useState, useContext } from "react";
+
+// import { getAuth } from "firebase/auth";
+import { db } from "../../../firebase-config";
+import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { AuthenticationContext } from "../authentication/authentication.context";
+
+export const QuizContext = createContext();
+export const QuizContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+  const [quizData, setQuizData] = useState([]);
+  const [score, setScore] = useState(0);
+
+  const quizCollection = collection(db, "quiz_sets");
+  //   useEffect(() => {
+  //     console.log("Quiz");
+  //     onSnapshot(doc(db, "quiz_sets"), () => {
+  //       console.log("Quiz data downloaded");
+  //     });
+  //   }, []);
+
+  return (
+    <QuizContext.Provider value={{ quizData, setQuizData, score, setScore }}>
+      {children}
+    </QuizContext.Provider>
+  );
+};
