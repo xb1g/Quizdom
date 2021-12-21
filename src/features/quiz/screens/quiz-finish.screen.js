d0632c09a2ec08f0ecca7c04176e7ff930d23ba1
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2021-12-21 19:41:49 +0700

    quiz finished

--- src/features/quiz/screens/quiz-finish.screen.js
+++ src/features/quiz/screens/quiz-finish.screen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Styl } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Text } from "../../../components/typography/text.component";
 import styled, { useTheme } from "styled-components/native";
 import { List } from "react-native-paper";
@@ -13,11 +13,51 @@ const QuizFinishItem = styled(List.Item)`
   margin-right: ${(props) => props.theme.space[3]};
   margin-left: ${(props) => props.theme.space[3]};
 `;
+
 export const QuizFinishScreen = ({ navigation }) => {
+  const theme = useTheme();
   const { score } = useContext(QuizContext);
+  var scorepg = (score * 20) / 100;
   return (
-    <View style={{ marginTop: 30, marginLeft: 30 }}>
-      <Text>FinishScreen {score}</Text>
-    </View>
+    <>
+      <Text
+        variant="label"
+        style={{
+          color: "white",
+          fontSize: 48,
+          paddingLeft: 20,
+          backgroundColor: "#ff66c4",
+          paddingTop: 20,
+          paddingBottom: 20,
+        }}
+      >
+        {"QUIZ" + " " + "Completed"}
+      </Text>
+      <View
+        style={{
+          backgroundColor: "#393939",
+        }}
+      >
+        <View style={{ marginTop: 30, marginHorizontal: 50 }}>
+          {/* <ProgressBar progress={scorepg} color="#a359a0" /> */}
+        </View>
+        <QuizFinishItem
+          style={{
+            marginTop: 50,
+            marginHorizontal: 50,
+            backgroundColor: "#ffffff",
+            borderRadius: 50,
+          }}
+          titleStyle={{
+            color: "#ffaadd",
+            fontSize: 20,
+            paddingHorizontal: 20,
+            paddingTop: 20,
+            paddingBottom: 20,
+          }}
+          title="Well done!"
+        />
+      </View>
+    </>
   );
 };
