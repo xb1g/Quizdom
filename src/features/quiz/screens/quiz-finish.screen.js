c857c0fe7e3e5c8354bfbd20509d6e00c9713e7d
Bunyasit Fang <big168bk@gmail.com>
2021-12-21 20:02:40 +0700

    just tryna pull

--- src/features/quiz/screens/quiz-finish.screen.js
+++ src/features/quiz/screens/quiz-finish.screen.js
@@ -2,18 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text } from "../../../components/typography/text.component";
 import styled, { useTheme } from "styled-components/native";
-import { List } from "react-native-paper";
+import { Button, List } from "react-native-paper";
 import { ScrollView } from "react-native-gesture-handler";
 import { Row } from "../../../components/utility/row.component";
 import { QuizContext } from "../../../services/quiz/quiz.context";
 import { SafeTop } from "../../../components/utility/safe-area.component";
-
-const QuizFinishItem = styled(List.Item)`
-  margin-top: ${(props) => props.theme.space[3]};
-  margin-bottom: ${(props) => props.theme.space[3]};
-  margin-right: ${(props) => props.theme.space[3]};
-  margin-left: ${(props) => props.theme.space[3]};
-`;
 
 export const QuizFinishScreen = ({ navigation }) => {
   const theme = useTheme();
@@ -37,28 +30,16 @@ export const QuizFinishScreen = ({ navigation }) => {
       </Text>
       <View
         style={{
+          flex: 1,
           backgroundColor: "#393939",
         }}
       >
         <View style={{ marginTop: 30, marginHorizontal: 50 }}>
           {/* <ProgressBar progress={scorepg} color="#a359a0" /> */}
         </View>
-        <QuizFinishItem
-          style={{
-            marginTop: 50,
-            marginHorizontal: 50,
-            backgroundColor: "#ffffff",
-            borderRadius: 50,
-          }}
-          titleStyle={{
-            color: "#ffaadd",
-            fontSize: 20,
-            paddingHorizontal: 20,
-            paddingTop: 20,
-            paddingBottom: 20,
-          }}
-          title="Well done!"
-        />
+        <Button onPress={() => navigation.navigate("Resource")}>
+          <Text style={{ color: "white" }}>Go to Resources</Text>
+        </Button>
       </View>
     </>
   );
