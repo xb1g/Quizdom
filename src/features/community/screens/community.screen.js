bb8603bc3a89762c40a40444c2a809497f31899e
Chocolate-road <88416892+Chocolate-road@users.noreply.github.com>
2022-01-19 21:45:36 +0700

    fix errors

--- src/features/community/screens/community.screen.js
+++ src/features/community/screens/community.screen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ScrollView, StatusBar, View } from "react-native";
 import {
   TouchableHighlight,
--- src/services/authentication/community/community.context.js
+++ src/services/authentication/community/community.context.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState, useContext } from "react";
-import { db } from "../../../firebase-config";
+import { db } from "../../../../firebase-config";
 import {
   collection,
   query,
@@ -9,8 +9,7 @@ import {
   onSnapshot,
   setDoc,
 } from "firebase/firestore";
-import { AuthenticationContext } from "../authentication/authentication.context";
-import { setsMapTemplate } from "../data/math/sets";
+import { AuthenticationContext } from "../authentication.context";
 export const CommunityContext = createContext();
 export const CommunityContextProvider = ({ children }) => {
   const { user } = useContext(AuthenticationContext);
@@ -40,6 +39,8 @@ export const CommunityContextProvider = ({ children }) => {
         memberData,
         setMemberData,
       }}
-    ></CommunityContext.Provider>
+    >
+      {children}
+    </CommunityContext.Provider>
   );
 };
